c799e3cffa7d914cda6f03bb00a938b9b8611598
jscs <jscs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-20 16:19:55 +0000

    smoothing the rough edges
    git-svn-id: https://plugins.svn.wordpress.org/pleasant-viewer/trunk@728930 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- template_entry_form.php
+++ template_entry_form.php
@@ -3,23 +3,23 @@
 		<input type="hidden" id="rendered_citations" name="rendered_citations" value="<?php echo strip_tags(stripslashes($_POST['rendered_citations'])); ?>" />
 
 		<div style="width: 30%; float: left;">
-			<input type="text" id="name" name="post_topic" size="60" style="width: 80%; margin: 5px;font-size:20px;cursor:pointer;" value="<?php echo strip_tags(stripslashes($_POST['post_topic'])); ?>" /><br>
+			<input type="text" id="name" name="post_topic" size="60" style="width: 80%; border-radius: 5px; margin: 5px;font-size:20px;cursor:pointer;" value="<?php echo strip_tags(stripslashes($_POST['post_topic'])); ?>" /><br>
 			<select name="post_category_id" style="margin: 5px; margin-left: 15px;"><?php echo $category_options; ?></select>
 		</div>
 
 		<div style="float: left; width: 60%;">
-			<textarea id="description" name="post_introduction" rows="3" cols="20" style="width: 80%;font-size:12px;color:grey;"><?php echo strip_tags(stripslashes($_POST['post_introduction'])); ?></textarea>
+			<textarea id="description" name="post_introduction" rows="3" cols="20" style="border-radius: 5px; width: 80%;font-size:12px;color:grey;"><?php echo strip_tags(stripslashes($_POST['post_introduction'])); ?></textarea>
 		</div>
 
 		<div style="clear: both; width: 30%; float: left; margin-top: 30px; overflow:auto;">    
 			<strong>Citations</strong><span id="detected_book">Book</span><br /><span id="tooltip">Type your citation and hit <b>enter</b>...</span><br />
-			<textarea name="post_citations" id="citations" rows="16" cols="30"><?php echo strip_tags(stripslashes($_POST['post_citations'])); ?></textarea>
+			<textarea name="post_citations" id="citations" style="border-radius: 5px;" rows="16" cols="30"><?php echo strip_tags(stripslashes($_POST['post_citations'])); ?></textarea>
 			<input style="clear: both;" type="submit" name="submit" value="Save / Share" class="btn btn-primary" />
 		</div>
 
 		<div style="width: 70%; float: right; margin-top: 30px;">    
 			<strong>Preview</strong><br />
-			<div id="sortable" style="height: 500px;overflow: auto; border: 1px solid #eee;" contenteditable="true"></div>		
+			<div id="sortable" style="height: 500px;overflow: auto; border-radius: 5px; border: 1px solid #eee;" contenteditable="true"></div>		
 		</div>
 
 	</form>
