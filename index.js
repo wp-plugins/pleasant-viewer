65989fb73524cf5d08e88211834186a166dd8e2e
jscs <jscs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-20 16:16:41 +0000

    cleanup
    git-svn-id: https://plugins.svn.wordpress.org/pleasant-viewer/trunk@728928 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 $j=jQuery.noConflict();
 $j(function(){
   function detect_book(citation_string){
-    var splitcitations;
-    var book;
-    var citation;
-    var api_string;
+    var splitcitations, book, citation, api_string;
     
-    splitcitations = citation_string.split(" ");
-    console.log(splitcitations);
+    splitcitations = citation_string.split(" ");    
     book = splitcitations[0];
-    citation = splitcitations.slice(1,splitcitations.size).join(' ');
-    console.log(book);
-    console.log(citation);
+    citation = splitcitations.slice(1,splitcitations.size).join(' ');    
     if (book.indexOf(':') == -1){
       if (book == "KJV") {api_string = 'bible ' + citation}
       else if (book == "YLT") {api_string = 'yltbible ' + citation}
@@ -25,6 +19,7 @@ $j(function(){
     }
     return { book: book, citation: citation, api_string: api_string }
   }
+
   function Citation(citation){
     that = this;
     this.ready = function(cb){
@@ -73,10 +68,7 @@ $j(function(){
     
     input_lines = $j('#citations').val().split('\n');
     for (var i=0;i<input_lines.length;i++){ 
-      var xc = '#xc' + i;
-      
-      //console.log($j(xc).val());
-      //console.log($j('#citations').val().split('\n')[i]);
+      var xc = '#xc' + i;            
       var new_input = $j('#citations').val().split('\n')[i];
       var cn = i + 1;
       if($j('#sortable > dl > input')[i]){
@@ -88,63 +80,49 @@ $j(function(){
       if (xcvalue != input_lines[i]) {
 	if (input_lines[i].length == 0) {
 	  if($j('#sortable > dl')[i]){
-	    el = $j('#sortable > dl > dt')[i];
-	    //$j(el).text('Citation #'+cn);
+	    el = $j('#sortable > dl > dt')[i];	    
 	    $j(el).text('');
 	    $j(el).append('<hr />');
-	    el = $j('#sortable > dl > dd')[i];
-	    //$j(el).text('Please type eg 4:4 or Gen 1:1...');
+	    el = $j('#sortable > dl > dd')[i];	 
 	    $j(el).text('');
 	  }
 	}
-	console.log('xcvalue: ' + xcvalue);
-	console.log('xcvaluelength: ' + xcvalue.length);
 	if (xcvalue == 'na') {	  
 	  $j('#sortable').append('<dl id="x'+i+'"><input type="hidden" id="xc'+i+'" class="xc" value="' + input_lines[i] + '"><dt id="citation_citation_'+i+'"><hr /></dt><dd id="citation_text_'+i+'"></dd> </dl>');      
-	}
-	//console.log("unequal");
+	}	
 	
 	$j(xc).val(input_lines[i]);
 	reload_citation(i);
       }            
     }        
     var i=1;
-    $j('#sortable > dl').each(function () {
-      //console.log('i' + i);
-      //console.log('max lines' + input_lines.length);
-      //console.log($j(this).val());
+    $j('#sortable > dl').each(function () {      
       if (i>input_lines.length) {
 	$j(this).remove();
       }
       i++;
       
     });
-    $j('#x' + current_line()).attr('style','background-color: #eeeeee;-moz-border-radius: 15px;border-radius: 15px;padding-left:8px;');
-    //alert($j('#sortable').html());	
+    $j('#x' + current_line()).attr('style','background-color: #eeeeee;-moz-border-radius: 15px;border-radius: 15px;padding-left:8px;');    
     scroll_preview();
   }
   
   function reload_citation(cl){
-    $j('#tooltip').text("Looking up citations...");    
-    //console.log(current_line());        
+    $j('#tooltip').text("Looking up citations...");        
     cit = $j('#citations').val().split('\n')[cl];
     
     $j('#detected_book').text('  Book: ' + detect_book(cit).book);    
           
     citation = new Citation(cit);
-  console.log(citation);  
     citation.ready(function(){  
 
-	citation.citation = detect_book(cit).book + ' ' + citation.citation
+    citation.citation = detect_book(cit).book + ' ' + citation.citation
 
-  console.log(citation);
-  console.log(citation.citation);
-  console.log(citation.text);
-      $j('#citation_citation_'+cl).text(citation.citation);
-     formatted_citation = format_citation(citation.text);
-      $j('#citation_text_'+cl).html(formatted_citation);        
-      $j('#tooltip').text("Done, hit enter for the next one...");
-      scroll_preview();
+    $j('#citation_citation_'+cl).text(citation.citation);
+    formatted_citation = format_citation(citation.text);
+    $j('#citation_text_'+cl).html(formatted_citation);        
+    $j('#tooltip').text("Done, hit enter for the next one...");
+    scroll_preview();
     });
     
   }
